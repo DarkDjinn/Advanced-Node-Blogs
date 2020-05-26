7e196845ac264995d825e96dce46a7eefd75f16d
DarkDjinn <darker.djinn@gmail.com>
2020-05-26 16:32:19 +0300

    Init Commit

---
+++ tests/setup.js
@@ -0,0 +1,8 @@
+jest.setTimeout(30000);
+
+require('../models/User');
+const mongoose = require('mongoose');
+const { mongoURI } = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, { useMongoClient: true });
